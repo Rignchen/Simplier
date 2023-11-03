ed744948ff855143b883406093ab2c73da7ec424
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-11-03 22:55:34 +0100

    define esolang

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Simplier is an esoteric programming language based on the concept of simplicity.
+Some of the features of this language include:
+- Small instruction names
+- First line is never executed and can be as long as you want
+- No repetition of lines (lines with more than one instruction can't be repeated)
+- Maximum of 60 characters per line
+- Lines must end with ; and everything after it is ignored but still counted for the 60 character limit
+- No complex data types
+- Numbers in variables are between 0 and 255, Floats are between -128 and 127
+- Character are given using their ASCII value
+This language is interpreted by the Simplier Interpreter, which is written in Python.
+"""
