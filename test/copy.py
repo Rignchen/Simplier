1973354d538309f8ee45217d6e9da343b11a9f52
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-11-07 13:40:01 +0100

    add self remove to generated test script

--- test/copy.py
+++ test/copy.py
@@ -16,10 +16,11 @@ files = [ # set -> in -> calcul -> bool -> if -> go -> function -> lib -> run
 	"run.simple",
 ]
 
-out = '#import\nfrom os import chdir, listdir, name, system\n\nsystem("cls" if name == "nt" else "clear")\n\n# if in main folder, move to test folder\nif "test" in listdir(): chdir("test")\n\n# test files\nfile_list = ['
+out = '#import\nfrom os import chdir, path, remove, name, system\n\nsystem("cls" if name == "nt" else "clear")\n\n# if in main folder, move to test folder\nchdir(path.dirname(path.realpath(__file__)))\n\n# test files\nfile_list = ['
 for i in files: out += f'\n\t"{i}",'
 out += '\n]\n\n# interpreter\nfor file_path in file_list:\n\tprint(f"now testing {file_path}")'
 
 for line in text: out += f"\n\t{line}"
+out += "\nremove(path.realpath(__file__))"
 
 with open("test/test_all.py","w+") as f: f.write(out)
