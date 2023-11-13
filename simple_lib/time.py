ab73647b3263dc360ec936a16d46d9aa7f709766
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-11-13 11:11:28 +0100

    Fix small error in time lib

--- simple_lib/time.py
+++ simple_lib/time.py
@@ -32,15 +32,15 @@ class _main:
 		#look for the function
 		match words[0]:
 			case "now": # get the current unix time
-				# syntaxe: now <varriable:3.14>
-				if len(words) > 2: self.error(f"now on line {self.curent_line} must have 1 arguments")
+				# syntaxe: now <varriable:42|3.14>
+				if len(words) != 2: self.error(f"now on line {self.curent_line} must have 1 arguments")
 				var = self.get_variable(words[1])
 				if var[1] == "3.14": self.variables[words[1]] = self.variables[words[1]] = ("3.14", self.get_value(str(time.time()), "3.14"))
 				elif var[1] == "42": self.variables[words[1]] = self.variables[words[1]] = ("42", self.get_value(str(int(time.time())), "42"))
 				else: self.error(f"{var[0]} on line {self.curent_line} must be a number")
 			case "zzz": # wait for x seconds
 				# syntaxe: zzz <varriable:42|3.14>
-				if len(words) > 2: self.error(f"zzz on line {self.curent_line} must have 1 arguments")
+				if len(words) != 2: self.error(f"zzz on line {self.curent_line} must have 1 arguments")
 				if words[1].startswith("$"): v = self.get_variable(words[1][1:])
 				else: v = [words[1], "3.14"]
 				test, msg = self.is_value_valid(v[0], v[1])
